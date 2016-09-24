24171d5bcc9e4f25f82411730ef5cf9750444c70
Byambatsog <byambatsog@brilent.com>
2016-09-24 09:40:45 -0500

    Initial project

---
+++ src/main/java/com/comp344/ecommerce/App.java
@@ -0,0 +1,13 @@
+package com.comp344.ecommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
